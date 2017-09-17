0745b243f628350a0db01399d90c682042ad3e19
Keith <kperkins411@gmail.com>
2017-09-17 14:08:43 -0400

    updated project1 for next time

--- content/projects/proj1/utilities.h
+++ content/projects/proj1/utilities.h
@@ -3,6 +3,11 @@
 *
 *  Created on: Aug 28, 2016
 *      Author: Keith
+*  part of project1 for OS
+*  Changelog
+*  9/17/17 sort - clarified meaning - low to high
+*          loadData - clarified behaviour - clears vector on load
+*          saveData - clarified behaviour - overwrite file contents
 */
 
 #ifndef UTILITIES_H_
@@ -27,6 +32,7 @@ enum SORT_ORDER { CPU_TIME, PROCESS_NUMBER, START_TIME };
 
 //allthe functions available for use
 
+//clears vector holding process_stats structs
 //attempt to open file 'filename' to read, parse its rows 
 //into process_stats structs and add these structs to a vector 
 //
@@ -36,15 +42,17 @@ int loadData(const char* filename);
 //attempt to open file 'filename' to write, and serialize a
 //vector full of process_stats structs to it.  Should be in the same 
 //format as original file but not necessarily the same order or length 
-//
+//if the file exists, overwrite its contents.
 //returns SUCCESS if all goes well or COULD_NOT_OPEN_FILE
 int saveData(const char* filename);
 
 //sorts the vector, returns nothing (thats what void means)
+//sorts low to high
 void sortData(SORT_ORDER mySortOrder);
 
 //return the first struct in the vector
 //then deletes it from the vector
 process_stats getNext();
+
 
 #endif /* UTILITIES_H_ */
