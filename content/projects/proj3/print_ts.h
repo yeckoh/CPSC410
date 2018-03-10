8f82ca14f5e4a0abec7da041eeda025ea9c53108
Keith <kperkins411@gmail.com>
2018-03-10 01:53:34 -0500

    proj

---
+++ content/projects/proj3/print_ts.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+
+void PRINT1(std::string &txt);
+void PRINT2(std::string &txt, std::string &txt1);
+void PRINT3(std::string &txt, std::string &txt1, std::string &txt2);
+void PRINT4(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3);
+void PRINT5(std::string &txt, std::string &txt1, std::string &txt2, std::string &txt3, std::string &txt4);
+
---
+++ content/projects/proj3/tester.h
@@ -0,0 +1,38 @@
+/*
+ * tester.h, 
+ *
+ *  Created on: Mar 9, 2018
+ *      Author: keith
+ */
+
+#ifndef TESTER_H_
+#define TESTER_H_
+
+enum WHICH_PRINT{P1,P2,P3,P4,P5};
+const std::string USER_CHOSE_TO_CANCEL = "User Chose To Cancel";
+
+/*
+ * starts cancelable threads
+ * string s			-the string to print
+ * numThreads 		-the number of threads to start
+ * wp 				-which print statement should be used
+ * numTimesToPrint 	-the number of PRINT statements each thread will call
+ * millisecond_delay- how long (in milliseconds) to wait between prints
+ * 
+ * if user chooses to cancel then this function should print USER_CHOSE_TO_CANCEL before exit
+ */
+void startThreads(std::string s, int numThreads, WHICH_PRINT wp, int numTimesToPrint, int millisecond_delay);
+
+/*
+ * if bCanceled== true then causes all running threads to exit
+ * if false then just reset logic used to cancel threads
+ */
+void setCancelThreads(bool bCancel);
+
+/*
+ * waits for all threads to complete
+ */
+void joinThreads();
+
+
+#endif /* TESTER_H_ */
