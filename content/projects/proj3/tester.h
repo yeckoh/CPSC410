4589c03ec7e055eb689522d696e2cb2354795b13
Keith Perkins <CNUClasses@users.noreply.github.com>
2018-03-21 12:08:44 -0400

    Update tester.h

--- content/projects/proj3/tester.h
+++ content/projects/proj3/tester.h
@@ -7,7 +7,6 @@
 
 #ifndef TESTER_H_
 #define TESTER_H_
-#include <string>
 
 enum WHICH_PRINT{P1,P2,P3,P4,P5};
 const std::string USER_CHOSE_TO_CANCEL = "User Chose To Cancel";
@@ -19,13 +18,11 @@ const std::string USER_CHOSE_TO_CANCEL = "User Chose To Cancel";
  * wp 				-which print statement should be used
  * numTimesToPrint 	-the number of PRINT statements each thread will call
  * millisecond_delay- how long (in milliseconds) to wait between prints
- * 
- * if user chooses to cancel then this function should print USER_CHOSE_TO_CANCEL before exit
  */
 void startThreads(std::string s, int numThreads, WHICH_PRINT wp, int numTimesToPrint, int millisecond_delay);
 
 /*
- * if bCanceled== true then causes all running threads to exit
+ * if bCanceled== true then causes all running threads to print USER_CHOSE_TO_CANCEL and then exit
  * if false then just reset logic used to cancel threads
  */
 void setCancelThreads(bool bCancel);
