f770a0df777f4763143dd52f4214351356686c92
Keith Perkins <CNUClasses@users.noreply.github.com>
2018-03-25 20:49:09 -0400

    Update print_ts.h

--- content/projects/proj3/print_ts.h
+++ content/projects/proj3/print_ts.h
@@ -1,6 +1,15 @@
 #pragma once
 #include <string>
 
+//create threadsafe implementations of the following functions
+//example
+//string s1 = "t";
+//string s2 = "s";
+//PRINT2(s1,s2);  
+//PRINT3(s1,s2,s1);
+//should output
+//ts
+//tst
 void PRINT1(std::string &txt);
 void PRINT2(std::string &txt, std::string &txt1);
 void PRINT3(std::string &txt, std::string &txt1, std::string &txt2);
